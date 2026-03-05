d639def20499b0aaa6ad3dbaf6967d3d6b4c86b0
Fliyanto <s4134108@student.rmit.edu.au>
2026-03-05 16:25:56 +1100

    hehe

---
+++ main.js
@@ -0,0 +1,19 @@
+// Create the Konva stage//
+
+const stage = new Konva.Stage({
+  container: "konva-stage",
+  width: 500,
+  height: 500,
+});
+
+const firstlayer = new Konva.Layer();
+
+const circle = new Konva.Circle({
+  x: 250,
+  y: 250,
+  radius: 50,
+  fill: "red",
+});
+
+firstlayer.add(circle);
+stage.add(firstlayer);
